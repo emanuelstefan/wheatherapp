5375ceaf8c55633e051738caae58361ba262563d
Emanuel Stefan <stefancostiemanuel@gmail.com>
2021-12-31 14:58:21 +0200

    retrieving and displaying weather info [WIP]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'wheather#index'
 
   resources :wheather, only: [:index]
+  get '/wheather/getweatherinfo', to: 'wheather#getweatherinfo'
+
   resources :temperature_configs, only: [:index, :new, :edit, :update, :create, :destroy]
 end
